52f877f607b964958d8f550ebbaf6ee032c73366
Mechanical Beaver <pyroasperin@gmail.com>
2025-07-06 21:20:58 +0500

    add json

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,20 @@
 #include <SD.h>
 
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 
+#include "ArduinoJson/Object/JsonObject.hpp"
 #include "HardwareSerial.h"
 #include "WString.h"
 #include "esp32-hal.h"
 
-// Lib                             //Typically                                   // PCM5102
-// SCK - Continuous serial clock   BCLK - bit clock                              // BCK
-// FS - Frame sync                 WS - Word Select = LRCLK - left rigth clock   // LCK
-// SD - Serial data
-// SDIN - data in                                                                // DIN
-// SDOUT - data out
+// Typically                                     // PCM5102
+// BCLK - bit clock                              // BCK
+// WS - Word Select = LRCLK - left rigth clock   // LCK
+// Dout                                          // DIN
 
-// DAC Dout 25, 26 (maybe)
-
-#define SD_cs 1
+#define SD_cs 10
 
 #define LED_pin 48
 #define LED_count 1
@@ -34,14 +33,29 @@
 #define US_max_dist 300
 
 CRGB leds[LED_count];
-
 Audio PCM5102;
-
 NewPing US_sensor(US_TRIG_pin, US_ECHO_pin, US_max_dist);
 
-void Led_on(uint8_t r, uint8_t g, uint8_t b);
-void Error404(String error_massage = "", bool serial_activ = true);
+File Json;
+const char* Json_name = "/config.json";
+StaticJsonDocument<256> Json_conf;
+DeserializationError Json_error;
+
+struct colors
+{
+    String err;
+    String success;
+    String play;
+};
+
+colors Cols;
+
+uint8_t _volume;
+
+void led_on(String hex_code);
+void error404(String error_massage = "", bool serial_activ = true);
 void play();
+void get_conf();
 
 void setup()
 {
@@ -50,20 +64,20 @@ void setup()
 
     if (!Serial)
     {
-        Error404("", false);
+        error404("", false);
     }
 
     if (!SD.begin(SD_cs))
     {
-        Error404("SD_card dont init");
+        error404("SD_card dont init");
     }
 
-    char inf;
+    get_conf();
 
     PCM5102.setPinout(BCLK, WS, Dout);
-    PCM5102.setVolume(10);
+    PCM5102.setVolume(_volume);
 
-    Led_on(0, 255, 0);
+    led_on(Cols.success);
 }
 
 void loop()
@@ -78,20 +92,48 @@ void loop()
     }
 }
 
-void Led_on(uint8_t r, uint8_t g, uint8_t b)
+void led_on(String hex_code)
 {
-    leds[0] = CRGB(r, g, b);
+    leds[0] = strtoul(hex_code.c_str(), 0, 16);
     FastLED.show();
 }
 
-void Error404(String error_massage, bool serial_activ)
+void error404(String error_massage, bool serial_activ)
 {
     if (serial_activ)
     {
         Serial.println(error_massage);
     }
-    Led_on(255, 0, 0);
+    led_on(Cols.err);
     while (true);
+}
+
+void get_conf()
+{
+    Json = SD.open(Json_name);
+    if (!Json)
+    {
+        error404("File not found");
+    }
+
+    Json_error = deserializeJson(Json_conf, Json);
+
+    Json.close();
+
+    if (Json_error)
+    {
+        char err[128];
+        sprintf(err, "Error: %s", Json_error.c_str());
+        error404(err);
+    }
+
+    JsonObject cols = Json_conf["Led_colors"];
+
+    Cols.err = cols["Error"].as<String>();
+    Cols.success = cols["Successful"].as<String>();
+    Cols.play = cols["Play"].as<String>();
+
+    _volume = Json_conf["Volume"];
 }
 
 void play()
