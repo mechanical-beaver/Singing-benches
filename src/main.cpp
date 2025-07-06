a866e5dd010c672a85fd8ad49217c1aade9fd3cd
Mechanical Beaver <pyroasperin@gmail.com>
2025-07-06 13:57:18 +0500

    add stop/reset

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 #include "HardwareSerial.h"
 #include "WString.h"
+#include "esp32-hal.h"
 
 // Lib                             //Typically                                   // PCM5102
 // SCK - Continuous serial clock   BCLK - bit clock                              // BCK
@@ -24,9 +25,9 @@
 #define LED_pin 48
 #define LED_count 1
 
-#define BCLK 4
-#define WS 5
-#define Dout 6
+#define BCLK 3
+#define WS 1
+#define Dout 9
 
 #define US_TRIG_pin 10
 #define US_ECHO_pin 11
@@ -40,6 +41,7 @@ NewPing US_sensor(US_TRIG_pin, US_ECHO_pin, US_max_dist);
 
 void Led_on(uint8_t r, uint8_t g, uint8_t b);
 void Error404(String error_massage = "", bool serial_activ = true);
+void play();
 
 void setup()
 {
@@ -56,16 +58,24 @@ void setup()
         Error404("SD_card dont init");
     }
 
+    char inf;
+
     PCM5102.setPinout(BCLK, WS, Dout);
-    PCM5102.setVolume(100);
-    PCM5102.connecttoFS(SD, "/test.mp3");
+    PCM5102.setVolume(10);
 
     Led_on(0, 255, 0);
 }
 
 void loop()
 {
-    PCM5102.loop();
+    if (Serial.available())
+    {
+        String key = Serial.readString();
+        if (key == "play")
+        {
+            play();
+        }
+    }
 }
 
 void Led_on(uint8_t r, uint8_t g, uint8_t b)
@@ -82,4 +92,43 @@ void Error404(String error_massage, bool serial_activ)
     }
     Led_on(255, 0, 0);
     while (true);
+}
+
+void play()
+{
+    bool status = true;
+    uint32_t duration;
+    bool dur_stat = false;
+    uint32_t timer1 = millis();
+
+    PCM5102.connecttoFS(SD, "/test.mp3");
+
+    while (status)
+    {
+        PCM5102.loop();
+
+        if (!dur_stat)
+        {
+            duration = PCM5102.getAudioFileDuration();
+            if (duration != 0)
+            {
+                dur_stat = true;
+            }
+        }
+
+        if (dur_stat && millis() - timer1 >= (duration + 3) * 1000)
+        {
+            status = false;
+        }
+
+        if (Serial.available())
+        {
+            String key = Serial.readString();
+            if (key == "stop")
+            {
+                status = false;
+            }
+        }
+    }
+    Serial.println("end");
 }
