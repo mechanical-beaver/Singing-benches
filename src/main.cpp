97c78343ce8629156c9566a8c307844bc63ace64
mechanical-beaver <pyroasperin@gmail.com>
2025-07-09 17:39:02 +0500

    add modes

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,15 @@
 #include "esp32-hal-gpio.h"
 #include "esp32-hal.h"
 
+//-----Mode-----
+#define SD_Card
+#define SPIFFS
+
+// #define UART
+// #define BT
+
+// #define LED
+
 //-----Pins-----
 #define SD_cs 10
 #define LED_pin 48
@@ -38,12 +47,6 @@
 // #define US_ECHO_pin 2
 // #define US_max_dist 300
 
-//-----BT_define-----
-#define BT_name "ESP32_Test"
-#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
-
 //-----OtherDefine-----
 #define LED_count 1
 
@@ -53,14 +56,8 @@ const char *Json_name = "/config.json";
 StaticJsonDocument<256> Json_conf;
 DeserializationError Json_error;
 
-//-----BLE_var-----
-BLEServer *pServer = nullptr;
-BLECharacteristic *pTxCharacteristic;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-bool BT_flag = false;
-
-//-----Leds_var-----
+//-----Leds-----
+#ifdef LED
 CRGB leds[LED_count];
 struct colors
 {
@@ -70,12 +67,26 @@ struct colors
     String restart;
 };
 colors Cols;
+void led_on(String hex_code);
+#endif
 
 //-----Other_var-----
 uint8_t _volume;
 uint32_t global_timer;
 
-//-----Classes-----
+#ifdef BT
+//-----BT-----
+#define BT_name "ESP32_Test"
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+BLEServer *pServer = nullptr;
+BLECharacteristic *pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+bool BT_flag = false;
+
 class MyServerCallbacks : public BLEServerCallbacks
 {
     void onConnect(BLEServer *pServer)
@@ -101,65 +112,89 @@ class MyCallbacks : public BLECharacteristicCallbacks
     }
 };
 
-//-----Object-----
-Audio PCM5102;
-// NewPing US_sensor(US_TRIG_pin, US_ECHO_pin, US_max_dist);
-
-//-----Func_init-----
-void led_on(String hex_code);
-void error404(String error_massage = "", bool serial_activ = true);
-void play();
-void get_conf();
-void rest();
 void BT_init();
 void BT_connect();
 void BT_disconnect();
 void Write_to_BT();
 
+#endif
+
+//-----Object-----
+Audio PCM5102;
+// NewPing US_sensor(US_TRIG_pin, US_ECHO_pin, US_max_dist);
+
+//-----Func_init-----
+#ifdef UART
+void error404(String error_massage = "", bool serial_activ = true);
+#endif
+void play();
+void get_conf();
+void rest();
+
 //-----Standart-----
 void setup()
 {
+#ifdef UART
     Serial.begin(115200);
-    pinMode(RST_pin, OUTPUT);
-    digitalWrite(RST_pin, 1);
-    FastLED.addLeds<WS2812, LED_pin, GRB>(leds, LED_count);
-    FastLED.setBrightness(100);
-
     if (!Serial)
     {
+#ifdef UART
         error404("", false);
+#else
+        while (1);
+#endif
     }
+
+#endif
+    pinMode(RST_pin, OUTPUT);
+    digitalWrite(RST_pin, 1);
+
+#ifdef LED
+    FastLED.addLeds<WS2812, LED_pin, GRB>(leds, LED_count);
+    FastLED.setBrightness(100);
+#endif
 
     if (!SD.begin(SD_cs))
     {
+#ifdef UART
         error404("SD_card dont init");
+#else
+        while (1);
+#endif
     }
 
     get_conf();
 
+#ifdef BT
     BT_init();
+#endif
 
     PCM5102.setPinout(BCK, LCK, DIN);
     PCM5102.setVolume(_volume);
 
+#ifdef LED
     led_on(Cols.success);
+#endif
 }
 
 void loop()
 {
-    // if (Serial.available())
-    // {
-    //     String key = Serial.readString();
-    //     if (key == "play")
-    //     {
-    //         play();
-    //     }
-    //     else if (key == "restart")
-    //     {
-    //         rest();
-    //     }
-    // }
+#ifdef UART
+    if (Serial.available())
+    {
+        String key = Serial.readString();
+        if (key == "play")
+        {
+            play();
+        }
+        else if (key == "restart")
+        {
+            rest();
+        }
+    }
+#endif
 
+#ifdef LED
     if (leds[0].r + leds[0].g + leds[0].b != 0)
     {
         if (millis() - global_timer >= 1000)
@@ -167,36 +202,47 @@ void loop()
             led_on("0x000000");
         }
     }
+#endif
 
-    BT_connect();
-    BT_disconnect();
-    Write_to_BT();
+    // BT_connect();
+    // BT_disconnect();
+    // Write_to_BT();
 }
 
 //-----Func_impl-----
+#ifdef LED
 void led_on(String hex_code)
 {
     leds[0] = strtoul(hex_code.c_str(), 0, 16);
     FastLED.show();
     global_timer = millis();
 }
+#endif
 
+#ifdef UART
 void error404(String error_massage, bool serial_activ)
 {
     if (serial_activ)
     {
         Serial.println(error_massage);
     }
+#ifdef LED
     led_on(Cols.err);
+#endif
     while (true);
 }
+#endif
 
 void get_conf()
 {
     Json = SD.open(Json_name);
     if (!Json)
     {
+#ifdef UART
         error404("File not found");
+#else
+        while (1);
+#endif
     }
 
     Json_error = deserializeJson(Json_conf, Json);
@@ -205,21 +251,28 @@ void get_conf()
 
     if (Json_error)
     {
+#ifdef UART
         char err[128];
         sprintf(err, "Error: %s", Json_error.c_str());
         error404(err);
+#else
+        while (1);
+#endif
     }
 
+#ifdef LED
     JsonObject cols = Json_conf["Led_colors"];
 
     Cols.err = cols["Error"].as<String>();
     Cols.success = cols["Successful"].as<String>();
     Cols.play = cols["Play"].as<String>();
     Cols.restart = cols["Restart"].as<String>();
+#endif
 
     _volume = Json_conf["Volume"];
 }
 
+#ifdef BT
 void BT_init()
 {
     BLEDevice::init(BT_name);
@@ -254,7 +307,9 @@ void BT_connect()
 {
     if (deviceConnected && !oldDeviceConnected)
     {
+#ifdef UART
         Serial.println("BLE client connected.");
+#endif
         oldDeviceConnected = deviceConnected;
     }
 }
@@ -271,7 +326,9 @@ void BT_disconnect()
         else if (millis() - global_timer >= 500)
         {
             pServer->startAdvertising();
+#ifdef UART
             Serial.println("BLE client disconnected. Start advertising...");
+#endif
             oldDeviceConnected = deviceConnected;
         }
     }
@@ -292,6 +349,7 @@ void Write_to_BT()
         }
     }
 }
+#endif
 
 void play()
 {
@@ -300,7 +358,9 @@ void play()
     bool dur_stat = false;
 
     PCM5102.connecttoFS(SD, "/test.mp3");
+#ifdef LED
     led_on(Cols.play);
+#endif
     while (status)
     {
         PCM5102.loop();
@@ -332,8 +392,12 @@ void play()
 
 void rest()
 {
+#ifdef UART
     Serial.println("Restarting...");
+#endif
+#ifdef LED
     led_on(Cols.restart);
+#endif
     delay(1000);
     digitalWrite(RST_pin, 0);
 }
